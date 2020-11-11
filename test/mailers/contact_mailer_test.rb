8ff0b2877e255dab34cfedc3356dccd782828072
James <trewern.james@gmail.com>
2020-11-11 12:41:14 -0800

    tests and fucntionaity added to contact mailer

--- test/mailers/contact_mailer_test.rb
+++ test/mailers/contact_mailer_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class ContactMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should return contact email" do
+    mail = ContactMailer.contact_emailil("matthew@me.com","Matthew Casey", "1234567890", @message = "Hello")
+    assert_equal['info@twerterbook.com'], mail.to
+    assert_equal['info@twerterbook.com'], mail.from
+  end
 end
