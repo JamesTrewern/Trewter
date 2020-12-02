8760836aeb4f2339dbcf670630ff889d6f2bb825
James <trewern.james@gmail.com>
2020-12-01 20:09:43 -0800

    added follow model validation and testing

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,15 @@
+class FollowerNotFollowed < ActiveModel::Validator
+  def validate(record)
+    if record.followed == record.follower
+      record.errors[:followed] << 'can not be the same as follower'
+      record.errors[:follower] << 'can not be the same as followed'
+    end
+  end
+end
+
 class Follow < ApplicationRecord
   belongs_to :follower, class_name: "Profile"
   belongs_to :followed, class_name: "Profile"
+  validates_with FollowerNotFollowed
+
 end
--- test/models/follow_test.rb
+++ test/models/follow_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class FollowTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @profile1 = profiles(:one)
+    @profile2 = profiles(:two)
+  end
+
+  test "should not save empty follow" do
+    follow = Follow.new
+    follow.save
+    refute follow.valid?
+  end
+
+  test "should not save self follow" do
+    follow = Follow.new({followed: @profile1, follower: @profile1})
+    follow.save
+    refute follow.valid?
+  end
+
+  test "should save valid follow" do
+    follow = Follow.new({followed: @profile1, follower: @profile2})
+    follow.save
+    assert follow.valid?
+  end
 end
