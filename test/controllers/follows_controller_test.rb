be3d892c2251527869a8bbe8ae5fa1705c4fac3d
JamesTrewern <trewern.james@gmail.com>
2020-12-12 12:40:59 -0800

    fixed follows controller tests

--- test/controllers/follows_controller_test.rb
+++ test/controllers/follows_controller_test.rb
@@ -1,48 +1,20 @@
 require 'test_helper'
 
 class FollowsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   setup do
-    @follow = follows(:one)
-  end
-
-  test "should get index" do
-    get follows_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_follow_url
-    assert_response :success
+    sign_in(users(:two))
   end
 
   test "should create follow" do
     assert_difference('Follow.count') do
-      post follows_url, params: { follow: { followed_id: @follow.followed_id, follower_id: @follow.follower_id } }
+      post follows_url, params: { follow: { followed_id: profiles(:three).id, follower_id: profiles(:two).id } }, xhr:true
     end
-
-    assert_redirected_to follow_url(Follow.last)
-  end
-
-  test "should show follow" do
-    get follow_url(@follow)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_follow_url(@follow)
-    assert_response :success
-  end
-
-  test "should update follow" do
-    patch follow_url(@follow), params: { follow: { followed_id: @follow.followed_id, follower_id: @follow.follower_id } }
-    assert_redirected_to follow_url(@follow)
   end
 
   test "should destroy follow" do
     assert_difference('Follow.count', -1) do
-      delete follow_url(@follow)
+      delete follow_url(follows(:one)), xhr: true
     end
-
-    assert_redirected_to follows_url
   end
 end
