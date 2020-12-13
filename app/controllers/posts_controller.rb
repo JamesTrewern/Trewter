a9abad0b6b11a98162e245b89fca2186bb66fd0c
JamesTrewern <trewern.james@gmail.com>
2020-12-12 16:19:51 -0800

    completed posts views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,17 +7,13 @@ class PostsController < ApplicationController
   def index
     @posts = Post.followed(current_user)
     @posts = @posts + current_user.profile.posts
+    @post = Post.new()
   end
 
   # GET /posts/1
   # GET /posts/1.json
   def show
 
-  end
-
-  # GET /posts/new
-  def new
-    @post = Post.new
   end
 
   # GET /posts/1/edit
